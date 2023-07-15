52a49c9c437815bf737e344c63b05c388db5db83
Daniel Miller <danielm@lola-post.com>
2023-07-15 10:21:57 +0100

    Changed gizmo to group. restored callbacks but on NoOp nested within group

--- GradeLightGroup_Node_Functions.py
+++ GradeLightGroup_Node_Functions.py
@@ -1,7 +1,22 @@
 import re
 import nuke
 
+# these look repetetive but the important thing is if we need to update in future from say "LGT" to "lgt" then they are all in one place!!
 LIGHT_GROUP_REGEX = re.compile(r'(LGT\d+)(_\w+_?\w+)')
+LIGHT_GROUP_FLAG_REGEX = re.compile(r'LGT\d+')
+LIGHT_GROUP_FLAG = 'LGT'
+
+
+aovs = [
+'diffuse_direct',
+'diffuse_indirect',
+'reflect_direct',
+'reflect_indirect',
+'refract_direct',
+'refract_indirect',
+'sss_direct',
+'sss_indirect'
+]
 
 def get_layers(thisNode):
     '''returns a list of all the layers '''
@@ -24,49 +39,20 @@ def get_aovs_per_lightgroup(node):
 
     return aovsPerLGSorted
 
-callback='''
 
-node = nuke.thisNode()
-k=nuke.thisKnob()
-if k.name() == 'lightgroup':
-    import re
-    lg = k.value()
-    lgRegex=re.compile(r'LGT\d+')
-    aovs = [
-    'diffuse_direct',
-    'diffuse_indirect',
-    'reflect_direct',
-    'reflect_indirect',
-    'refract_direct',
-    'refract_indirect',
-    'sss_direct',
-    'sss_indirect'
-    ]
+def remove_lg_knobs():
 
-    def update_aov_names(lgRegex):
-        """
-        Will update all the Shuffles, ShuffleCopys and Merge/From nodes to the correct lightgroup
-        Will look for knobtypes ['in1','in2','out1','out2','Achannels','Bchannels'] within ['Merge2','Shuffle2','Shuffle','ShuffleCopy','ChannelMerge'] to update
-        """
-        knobTypes=['in','in1','in2','out1','out2','Achannels','Bchannels']
-        with node:
-            message=''
-            for innerNode in nuke.allNodes():
-                if innerNode.Class() in ['Merge2','Shuffle2','Shuffle','ShuffleCopy','ChannelMerge']:
-                    for knob in innerNode.knobs():
-                        if knob in knobTypes:
-                            oldLayer=innerNode[knob].value()
-                            newLayer=re.sub(lgRegex, lg, oldLayer)
-                            innerNode[knob].setValue(newLayer)
-    update_aov_names(lgRegex)
-    print ('Updated Callback')
+    # Get the my_knob and remove button.
+    knob_name = nuke.thisKnob().name()[:-7]
+    knob_name_remove = nuke.thisKnob()
 
-'''
+    # Delete them!
+    nuke.thisNode().removeKnob(nuke.thisNode().knob(knob_name))
+    nuke.thisNode().removeKnob(knob_name_remove)
 
-#callback='GradeLightGroup_Node.grade_lg_node_callback()'
-
-
-#node = nuke.thisNode()
-
-#LGdropDownItems=list ( get_aovs_per_lightgroup(node).keys() )
-#node['lightgroup'].setValues(LGdropDownItems)
+    # Dive into the group and find the node with the label that matches the knobs that were
+    # just removed. DELETE THE NODE!
+    nuke.thisNode().begin()
+    for node in nuke.allNodes():
+        if knob_name in node.knob('label').value():
+            nuke.delete(node)
